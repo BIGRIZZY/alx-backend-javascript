ad2492716aba654e606a85d9c16cbf46ee8818f5
RIZZY'S WORLD <111015539+BIGRIZZY@users.noreply.github.com>
2023-08-21 11:03:28 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author oluwole adebanjo <https://github.com/B3zaleel>
+ * @author oluwole adebanjo <https://github.com/BIGRIZZY>
  */
 const displayMessage = (msg) => {
   console.log(msg);
